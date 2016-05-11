ee1f1d1268143aef213a836c89df5c8168f15509
Naldson Chagas <naldson.bc@gmail.com>
2016-05-11 07:44:22 -0300

    Terminando página inicial e criando página de preço de combustivel

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!--
-    Tela de auxílio de escolha entre gasolina e alcool;    
--->
+<?php
+function title() {
+    return "Controle do seu automóvel - Sistema de auxílio de preços de abastecimento";
+}
+require_once "_comum/header.php";
+?> 
+    <h2 class="h2 text-center" style="margin-top:70px;">Controle de automóvel</h2>
+    <h4 class=" h4 text-muted text-center">Sistema de auxílio de preços com manutenção e abastecimento</h4>    
 
 <?php
-    function title() {
-        return "Controle do seu automóvel";
-    }
-    require_once "_comum/header.php";
-?> 
+require_once "footer.php";
+?>
