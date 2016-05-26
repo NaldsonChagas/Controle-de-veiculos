d0e682fc702d819fede26fe5cd5227b7245b1ed2
Naldson <naldson.bc@gmail.com>
2016-05-26 01:38:55 -0300

    Terminando forrmulario de relatórios de abastecimentos

--- supply.php
+++ supply.php
@@ -15,7 +15,32 @@ require_once "_comum/header.php";
 		<br>
         
         <form>
+            <div class="form-group">
+                <label for="date">Data</label>
+                <input type="text" name="date" class="form-control" placeholder=" Data" id="date"/>
+            </div>
             
+            <div class="form-group">
+                <label for="fuel">Combustivel</label>
+                <select class="form-control">
+                    <option value="1">Álcool</option>
+                    <option value="2">Gasolina</option>
+                </select>
+            </div>
+            
+            <div class="form-group">
+                <label for="price">Valor</label>
+                <input type="number" name="price" class="form-control" placeholder=" Valor" id="price"/>
+            </div>
+            
+            <div class="form-group">
+                <label for="gas_station">Nome do posto</label>
+                <input type="text" name="gas_station" class="form-control" placeholder=" Nome do posto" id="gas_station"/>
+            </div>
+            
+            <div class="form-group">
+                <button type="submit" class="btn-primary btn">Salvar&nbsp;&nbsp;<span class="glyphicon glyphicon-ok"></span></button>
+            </div>
         </form>
     </div>
 </div>
